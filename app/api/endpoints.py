769b6b190fcd827b366db70045a3f5c1b95e9fc2
ribondareva <msbondareva01@gmail.com>
2025-04-09 21:55:17 +0300

    Created endpoints

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,32 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from api.schemas import EventSchema
 from deduplicator.bloom_filter import Deduplicator
+from deduplicator.producer import send_event_to_kafka
 
 router = APIRouter()
 
+deduplicator = Deduplicator()
 
-# Ручка для healthcheck
+
 @router.get("/health")
 def health_check():
     return {"status": "ok"}
 
 
-# Инициализация объекта Deduplicator
-deduplicator = Deduplicator()
+@router.on_event("startup")
+async def startup():
+    await deduplicator.init_redis()
 
 
-# Ручка для обработки событий
 @router.post("/event")
-async def process_event(event: EventSchema):  # Используем EventSchema для валидации
-    await deduplicator.init_redis()  # Инициализация Redis (можно переместить в init)
-
-    # Получаем item_id из нового события
-    item_id = event.product_id  # Предполагаем, что product_id — это идентификатор события
+async def process_event(event: EventSchema):
+    item_id = event.product_id
+    if not item_id:
+        raise HTTPException(status_code=400, detail="Missing product_id")
 
     is_unique = await deduplicator.is_unique(item_id)
 
     if is_unique:
         await deduplicator.add_to_bloom(item_id)
-        return {"message": "Event added and is unique", "item_id": item_id}
+        await send_event_to_kafka(event)
+        return {"message": "Event is unique and sent to Kafka", "item_id": item_id}
     else:
-        return {"message": "Event is not unique", "item_id": item_id}
+        raise HTTPException(status_code=400, detail=f"Event with item_id {item_id} is not unique")
