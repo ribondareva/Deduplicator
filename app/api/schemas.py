f06f6d8cdd3b38b276a9385af710a0a600712c29
ribondareva <msbondareva01@gmail.com>
2025-04-09 21:55:26 +0300

    Created schemas

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,19 +1,18 @@
 # Pydantic-схемы для валидации событий
-from pydantic import BaseModel, conint
+from pydantic import BaseModel
 from typing import Optional, Any, Dict
+from datetime import datetime
 
 
 class EventSchema(BaseModel):
     product_id: Optional[str]
-    event_datetime: Optional[str]
+    event_datetime: Optional[datetime]
     client_id: Optional[int]
     event_name: Optional[str]
-    inserted_dt: Optional[int]
-    sid: Optional[conint(strict=True)]
-    r: Optional[conint(strict=True)]
-
-    # Дополнительные поля
+    inserted_dt: Optional[datetime]
+    sid: Optional[str]
+    r: Optional[str]
     additional_fields: Optional[Dict[str, Any]] = {}
 
     class Config:
-        extra = "allow"  # позволяет пропускать все прочие поля, которые мы не описали явно
+        extra = "allow"  # позволяет пропускать все прочие поля
