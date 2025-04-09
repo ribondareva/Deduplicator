e3abf41387c24e8e668e0b1b757acf6ae6a37078
ribondareva <msbondareva01@gmail.com>
2025-04-09 21:56:34 +0300

    Created main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from api.endpoints import router as api_router
+from deduplicator.bloom_filter import Deduplicator
+from deduplicator.producer import init_kafka_producer, close_kafka_producer
 
 main_app = FastAPI(title="Deduplication Service")
 
 main_app.include_router(api_router)
+
+deduplicator = Deduplicator()
+
+
+@main_app.on_event("startup")
+async def startup():
+    await init_kafka_producer()
+
+
+@main_app.on_event("shutdown")
+async def shutdown():
+    await close_kafka_producer()
 
 if __name__ == "__main__":
     import uvicorn
