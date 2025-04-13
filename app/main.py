5ea935e5280b7888afce47108e37891f1c5b41f0
ribondareva <msbondareva01@gmail.com>
2025-04-13 21:50:29 +0300

    Added delete old events (celery task)

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from api.endpoints import router as api_router
 from deduplicator.bloom_filter import Deduplicator
 from deduplicator.producer import init_kafka_producer, close_kafka_producer
+from tasks import purge_old_events
 
 main_app = FastAPI(title="Deduplication Service")
 
@@ -13,6 +14,8 @@ deduplicator = Deduplicator()
 @main_app.on_event("startup")
 async def startup():
     await init_kafka_producer()
+    # Запускаем задачу purge_old_events через 24 часа (3600 * 24 секунд)
+    purge_old_events.apply_async(countdown=3600 * 24)
 
 
 @main_app.on_event("shutdown")
