b3dc27d53d755280c4e773130b8f56c75569ea6b
ribondareva <msbondareva01@gmail.com>
2025-04-20 15:40:43 +0300

    Refactoring

--- wait_for_kafka.sh
+++ wait_for_kafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Waiting for Kafka brokers..."
+echo "Ожидание Kafka брокеров..."
 until kafka-broker-api-versions.sh --bootstrap-server kafka1:9093 >/dev/null 2>&1; do
   sleep 2
 done
@@ -13,4 +13,4 @@ until kafka-broker-api-versions.sh --bootstrap-server kafka3:9095 >/dev/null 2>&
   sleep 2
 done
 
-echo "Kafka brokers are ready!"
+echo "Все Kafka брокеры доступны!"
