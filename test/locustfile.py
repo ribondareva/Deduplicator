9c81e58f90ff21caae8632aba9aa9b9e29bc0927
ribondareva <msbondareva01@gmail.com>
2025-04-22 19:22:15 +0300

    Locust testing added

--- test/locustfile.py
+++ test/locustfile.py
@@ -1,11 +1,3 @@
-# from locust import HttpUser, task, between
-#
-#
-# class TestUser(HttpUser):
-#     wait_time = between(1, 2)
-#     @task
-#     def healthcheck(self):
-#         self.client.get("health")
 from locust import HttpUser, task, between, events
 import time
 import uuid
